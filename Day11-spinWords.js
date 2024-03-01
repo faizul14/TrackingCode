f19e37b7a55cf03f374c0ea85d28368d77037f8a
Faezol <fpcyber61@gmail.com>
2024-03-02 06:22:41 +0800

    Day 11/366

--- Day11-spinWords.js
+++ Day11-spinWords.js
@@ -2,6 +2,15 @@
 // Day 10/366
 // https://www.codewars.com/kata/5264d2b162488dc400000001
 
-const spinWords = (string) => string.split(' ').map(a => a.length >= 5 ? a.split('').reverse().join('') : a).join(' ');
+// const spinWords = (string) => string.split(' ').map(a => a.length >= 5 ? a.split('').reverse().join('') : a).join(' ');
+
+// insight baru memudahkan pembacaan code
+const spinWords = (string) => {
+    return string
+        .split(' ')
+        .map(a => a.length < 5 ? a : a.split('').reverse().join(''))
+        .join(' ')
+}
+
 
 console.log(spinWords('Hey fellow warriors'));
