d690bd90c77e762d9b5a5dc7083456d69a03de98
Faezol <fpcyber61@gmail.com>
2024-03-09 07:29:54 +0800

    Day 14/366 - fixing logic done

--- Day14-persistence.js
+++ Day14-persistence.js
@@ -2,21 +2,31 @@
 // Day 14/366 => this chalenge still not finish (maybe i am need a time for think)
 // https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/
 
-const persistence = (num) => {
-    //code me
-    let resultCount = 0
-    let bantu = num.toString().split('')
-    // console.log(bantu);
+// const persistence = (num) => {
+//     //code me
+//     let resultCount = 0
+//     if (num < 10) {
+//         return resultCount
+//     }
+//     do {
+//         const bantu = num.toString().split('')
+//         for (let i = 0; i < bantu.length; i++) {
+//             num = i === 0 ? bantu[0] : num * bantu[i]
+//         }
+//         resultCount++
+//     } while (num >= 10);
+//     return resultCount
+// }
 
-    if (num < 10) {
-        return resultCount
+// insight baru dari dikusi
+const persistence = num => {
+    let times = 0;
+    num = num.toString();
+    while (num.length > 1) {
+        times++;
+        num = num.split('').map(a => parseInt(a)).reduce((acc, el) => acc * el).toString();
     }
-    // console.log(num);
-    // while(true){
-    //     if(bantu.reduce((acc, el) => acc *= el , 0) )
-    // }
-
-    return resultCount
+    return times;
 }
 
-console.log(persistence(39));
+console.log(persistence(999));
