cc413204a6e4bbda055540b4f206c1543bbf30d5
Faezol <fpcyber61@gmail.com>
2024-03-16 21:10:51 +0800

    Day 19/366

---
+++ Day19-century.js
@@ -0,0 +1,7 @@
+// FMP Track Coding Chalenge 2024
+// Day 19/366
+// https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/  
+
+const century = (year) => Math.ceil(year / 100)
+
+console.log(century(1705));
