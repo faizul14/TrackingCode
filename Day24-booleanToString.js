052d352d26ad8d34125924598d9b1d01746e974a
Faezol <fpcyber61@gmail.com>
2024-03-23 04:46:10 +0800

    Day 24/366

---
+++ Day24-booleanToString.js
@@ -0,0 +1,12 @@
+// FMP Track Coding Chalenge 2024
+// Day 24/366
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/
+
+// function booleanToString(b) {
+//     //your code here
+//     return b.toString()
+// }
+
+const booleanToString = b => b.toString()
+
+console.log(booleanToString(true));
