c547fca1ad0da3b7da84a549e9b25a0a1a6a4d59
Faezol <fpcyber61@gmail.com>
2024-03-29 20:04:52 +0800

    Day 28/366

---
+++ Day28-greet.js
@@ -0,0 +1,9 @@
+// FMP Track Coding Chalenge 2024
+// Day 28/366
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/
+
+const greet = (name) => `Hello, ${name} how are you doing today?`
+
+console.log(
+    greet('faezol')
+);
