7abba92f2aa83d4d8f7cdbc5ce8a9f6d8ec3b8e5
Faezol <fpcyber61@gmail.com>
2024-04-28 17:51:21 +0800

    Day 39/366: Still need fixing

--- Day39-orderWeight.js
+++ Day39-orderWeight.js
@@ -29,7 +29,7 @@ function orderWeight(strng) {
         const arrHelper = []
         if (arrayOrderObject[i].valueWeight === arrayOrderObject[i + 1].valueWeight) {
             arrHelper.push(...[arrayOrderObject[i], arrayOrderObject[i + 1]])
-            
+
             arrHelper.sort((a, b) => a.valueOriginal[0] - b.valueOriginal[0])
             // arrHelper
             arrayOrderObject.splice(i, 2)
@@ -44,23 +44,26 @@ function orderWeight(strng) {
     return result.join(' ')
 }
 
-
+// 13 105 170 109 163 28 191 29 85 68 12…
 
 console.log(
-    orderWeight(
-        '2000 10003 1234000 44444444 9999 11 11 22 123'
-    ),
+    // orderWeight(
+    //     '2000 10003 1234000 44444444 9999 11 11 22 123'
+    // ),
     // sumElemen(
     //     '103'
     // )
+    orderWeight(
+        '390883 163 163243 170 303671 85 337257 13 450763 28 326362 68 46607 168 125322 109 261501 105 282736 191 29'
+    )
 );
 
-const test = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-test.push(...[10, 11])
-test.splice(0, 2)
-test
+// const test = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// test.push(...[10, 11])
+// test.splice(0, 2)
+// test
 
-test.splice(0, 0, ...[2, 3])
-console.log(
-    test
-);
+// test.splice(0, 0, ...[2, 3])
+// console.log(
+//     test
+// );
