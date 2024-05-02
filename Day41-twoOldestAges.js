0d4cb8ae5aea2f7f8b498412915e9af6982bdad3
Faezol <fpcyber61@gmail.com>
2024-05-02 16:41:51 +0800

    Day 41/366

---
+++ Day41-twoOldestAges.js
@@ -0,0 +1,17 @@
+// FMP Track Coding Chalenge 2024
+// Day 41/366
+// https://www.codewars.com/kata/511f11d355fe575d2c000001/
+
+// function twoOldestAges(ages) {
+//     ages = ages.sort((a, b) => a - b)
+//     ages
+//     return [ages[ages.length - 2], ages[ages.length - 1]]
+// }
+
+const twoOldestAges = (ages) => ages.sort((a, b) => a - b).splice(-2)
+
+console.log(
+    twoOldestAges(
+        [1, 5, 87, 45, 8, 8]
+    )
+);
