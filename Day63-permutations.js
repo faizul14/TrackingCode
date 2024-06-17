5c1e6c325ba3695ff43e10c2766b96c27cd7f43f
Faezol <fpcyber61@gmail.com>
2024-06-17 19:05:06 +0800

    Day 63/366: nedd fixing

---
+++ Day63-permutations.js
@@ -0,0 +1,11 @@
+// FMP Track Coding Chalenge 2024
+// Day 63/366 => Ned Fixing
+// https://www.codewars.com/kata/5254ca2719453dcc0b00027d/
+
+function permutations(string) {
+    return [];
+}
+
+console.log(
+    'aabb'
+);
