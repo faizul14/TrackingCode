ea3faccf545a3ce1a129474b7d7c322f704c88a5
Faezol <fpcyber61@gmail.com>
2024-07-15 07:12:25 +0800

    Day 73/366

--- Day73-alphabetWar.js
+++ Day73-alphabetWar.js
@@ -2,37 +2,37 @@
 // Day 73/366
 // https://www.codewars.com/kata/59377c53e66267c8f6000027/
 
-// const leftSideLetterPower = {
-//    w: 4,
-//    p: 3,
-//    b: 2,
-//    s: 1,
-// }
-// const rightSideLetterPower = {
-//    m: 4,
-//    q: 3,
-//    d: 2,
-//    z: 1,
-// }
-// const leftSideLetterSoldier = 'wpbs'
-// const rightSideLetterSoldier = 'mqdz'
+const leftSideLetterPower = {
+   w: 4,
+   p: 3,
+   b: 2,
+   s: 1,
+}
+const rightSideLetterPower = {
+   m: 4,
+   q: 3,
+   d: 2,
+   z: 1,
+}
+const leftSideLetterSoldier = 'wpbs'
+const rightSideLetterSoldier = 'mqdz'
 
-// const letterFightWhoWin = (letterSoldier, leftOrRight) => {
-//    return letterSoldier
-//       .reduce((acc, el) => acc += leftOrRight[el], 0)
-// }
+const letterFightWhoWin = (letterSoldier, leftOrRight) => {
+   return letterSoldier
+      .reduce((acc, el) => acc += leftOrRight[el], 0)
+}
 
-// const clasificationSoldier = (soldier, leftOrRight) => soldier.split('').filter(el => leftOrRight.includes(el))
+const clasificationSoldier = (soldier, leftOrRight) => soldier.split('').filter(el => leftOrRight.includes(el))
 
-// function alphabetWar(fight) {
-//    let rightSoldier = clasificationSoldier(fight, rightSideLetterSoldier)
-//    let leftSoldier = clasificationSoldier(fight, leftSideLetterSoldier)
-//    rightSoldier = letterFightWhoWin(rightSoldier, rightSideLetterPower)
-//    leftSoldier = letterFightWhoWin(leftSoldier, leftSideLetterPower)
+function alphabetWar(fight) {
+   let rightSoldier = clasificationSoldier(fight, rightSideLetterSoldier)
+   let leftSoldier = clasificationSoldier(fight, leftSideLetterSoldier)
+   rightSoldier = letterFightWhoWin(rightSoldier, rightSideLetterPower)
+   leftSoldier = letterFightWhoWin(leftSoldier, leftSideLetterPower)
 
-//    // return "Let's fight again!";
-//    return leftSoldier == rightSoldier ? "Let's fight again!" : rightSoldier > leftSoldier ? 'Right side wins!' : 'Left side wins!'
-// }
+   // return "Let's fight again!";
+   return leftSoldier == rightSoldier ? "Let's fight again!" : rightSoldier > leftSoldier ? 'Right side wins!' : 'Left side wins!'
+}
 
 
 // get this solution on discussion, and very clever, i like it
