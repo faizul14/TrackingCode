6c94c44bc7242f8a6160f8876ad577cc0f011510
Faezol <fpcyber61@gmail.com>
2024-07-25 23:05:19 +0800

    fix Test Case

--- __tests__/Day74-fizzbuzz.test.js
+++ __tests__/Day74-fizzbuzz.test.js
@@ -1,7 +1,8 @@
 const { fizzbuzz } = require('../Day74-fizzbuzz')
+
 describe('FizzBuuzz', () => {
     test('Unit test for FizzBuuzz', () => {
-        const expect = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz']
-        expect(fizzbuzz(10)).toBe(expect);
+        expect(fizzbuzz(10)).toEqual([1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz']);
+        expect(fizzbuzz(3)).toEqual([1, 2, 'Fizz']);
     });
 });
