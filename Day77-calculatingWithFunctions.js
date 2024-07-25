8ee9438c88cb16d1974a37e62fabf4f5cabef18a
Faezol <fpcyber61@gmail.com>
2024-07-25 21:55:54 +0800

    Day 77/366

--- Day77-calculatingWithFunctions.js
+++ Day77-calculatingWithFunctions.js
@@ -2,20 +2,20 @@
 // Day 77/366
 // https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/
 
-const operationsValue = (operations = 'noValue', originalValue) => {
-    return /noValue/.test(operations) ? originalValue : Math.ceil(eval(originalValue.concat(operations)))
+const operationsValue = (operations = 'nova', originalValue) => {
+    return /nova/.test(operations) ? originalValue : Math.ceil(eval(originalValue.concat(operations)))
 }
 
-function zero(operations = 'noValue') { return operationsValue(operations, '0') }
-function one(operations = 'noValue') { return operationsValue(operations, '1') }
-function two(operations = 'noValue') { return operationsValue(operations, '2') }
-function three(operations = 'noValue') { return operationsValue(operations, '3') }
-function four(operations = 'noValue') { return operationsValue(operations, '4') }
-function five(operations = 'noValue') { return operationsValue(operations, '5') }
-function six(operations = 'noValue') { return operationsValue(operations, '6') }
-function seven(operations = 'noValue') { return operationsValue(operations, '7') }
-function eight(operations = 'noValue') { return operationsValue(operations, '8') }
-function nine(operations = 'noValue') { return operationsValue(operations, '9') }
+const zero = (operations = 'nova') => operationsValue(operations, '0')
+const one = (operations = 'nova') => operationsValue(operations, '1')
+const two = (operations = 'nova') => operationsValue(operations, '2')
+const three = (operations = 'nova') => operationsValue(operations, '3')
+const four = (operations = 'nova') => operationsValue(operations, '4')
+const five = (operations = 'nova') => operationsValue(operations, '5')
+const six = (operations = 'nova') => operationsValue(operations, '6')
+const seven = (operations = 'nova') => operationsValue(operations, '7')
+const eight = (operations = 'nova') => operationsValue(operations, '8')
+const nine = (operations = 'nova') => operationsValue(operations, '9')
 
 function plus(value) { return '+'.concat(value) }
 function minus(value) { return '-'.concat(value) }
@@ -23,7 +23,7 @@ function times(value) { return '*'.concat(value) }
 function dividedBy(value) { return '/'.concat(value) }
 
 console.log(
-    // seven(times(five()))
-    three(dividedBy(six))
+    seven(times(five()))
+    // three(dividedBy(six))
 );
 
