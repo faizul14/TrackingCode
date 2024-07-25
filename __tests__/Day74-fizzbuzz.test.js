aed95a613cd879388dc9a648091dd0c422123a54
Faezol <fpcyber61@gmail.com>
2024-07-25 23:45:11 +0800

    fix: Test Case make failed

--- __tests__/Day74-fizzbuzz.test.js
+++ __tests__/Day74-fizzbuzz.test.js
@@ -3,7 +3,7 @@ const { fizzbuzz } = require('../Day74-fizzbuzz')
 describe('FizzBuuzz', () => {
     test('Unit test for FizzBuuzz', () => {
         expect(fizzbuzz(10)).toEqual([1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz']);
-        expect(fizzbuzz(3)).toEqual([1, 2, 'Fizz']);
-        // expect(fizzbuzz(3)).toEqual([1, 2]);
+        // expect(fizzbuzz(3)).toEqual([1, 2, 'Fizz']);
+        expect(fizzbuzz(3)).toEqual([1, 2]);
     });
 });
