2bf18d1aeea20f728aa4ea37716bf5d326dbdf43
Faezol <fpcyber61@gmail.com>
2024-11-02 05:48:06 +0800

    Day 117/366

---
+++ Day117-digits.js
@@ -0,0 +1,15 @@
+// FMP Track Coding Chalenge 2024
+// Day 117/366
+// https://www.codewars.com/kata/58fa273ca6d84c158e000052/
+
+// function digits(n) {
+//     // TODO
+//     const digitsByParam = n.toString().length
+//     return digitsByParam
+// }
+
+const digits = n => n.toString().length
+
+console.log(
+    digits(12345)
+);
