facf229300da110120465b9e8762d28b86ff1646
Faezol <fpcyber61@gmail.com>
2024-11-23 10:04:32 +0800

    Day 130/366

--- Day130-topThreeWords.js
+++ Day130-topThreeWords.js
@@ -38,18 +38,18 @@ function topThreeWords(text) {
 }
 
 console.log(
-    //     topThreeWords(
-    //         `In a village of La Mancha, the name of which I have no desire to call to
-    //  mind, there lived not long since one of those gentlemen that keep a lance
-    //  in the lance-rack, an old buckler, a lean hack, and a greyhound for
-    //  coursing. An olla of rather more beef than mutton, a salad on most
-    //  nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
-    //  on Sundays, made away with three-quarters of his income.`
-    //     )
-    // topThreeWords("a a a  b  c c  d d d d  e e e e e"),
-    // topThreeWords("a a a  b  c c  d d d d  e e e e e"),
-    // topThreeWords("a a a c b b"),
-    // topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"),
+    topThreeWords(
+        `In a village of La Mancha, the name of which I have no desire to call to
+     mind, there lived not long since one of those gentlemen that keep a lance
+     in the lance-rack, an old buckler, a lean hack, and a greyhound for
+     coursing. An olla of rather more beef than mutton, a salad on most
+     nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+     on Sundays, made away with three-quarters of his income.`
+    ),
+    topThreeWords("a a a  b  c c  d d d d  e e e e e"),
+    topThreeWords("a a a  b  c c  d d d d  e e e e e"),
+    topThreeWords("a a a c b b"),
+    topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"),
     topThreeWords("  //wont won't won't "),
     topThreeWords("  , e   .. "),
     topThreeWords("  ...  "),
