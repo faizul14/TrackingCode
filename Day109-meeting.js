f5a544ea4a0be4e5ee09f1b0b2099ac4338b9797
Faezol <fpcyber61@gmail.com>
2025-06-11 19:05:01 +0800

    fix: Day 109/366

--- Day109-meeting.js
+++ Day109-meeting.js
@@ -2,48 +2,47 @@
 // Day 109/366 => Need fixing
 // https://www.codewars.com/kata/59df2f8f08c6cec835000012/
 
-const sortByFirstName = data => {
-    const resultAfterSort = []
-    let temp = []
-    data.map((el, idx, arr) => {
-        temp.push(el)
-        if ((idx !== arr.length - 1) && el !== temp[temp.length - 1] && el[0].charCodeAt() === temp[temp.length - 1][0].charCodeAt()) {
-            temp.push(el)
-        }else{
-            resultAfterSort.push(temp)
-            // temp = []
-        }
-    })
-
-    temp
-    resultAfterSort
-
-    const result = data.sort((a, b) => a[1].charCodeAt() - b[1].charCodeAt())
-
-    result
-
-}
-
 function meeting(s) {
-    // your code
-    s = s
+    return s
+        .toUpperCase()
         .split(';')
-        .map(el => el.toUpperCase().split(':').reverse())
-        .sort((a, b) => a[0].charCodeAt() - b[0].charCodeAt())
-    s
-    return sortByFirstName(s)
+        .map(name => {
+            const [first, last] = name.split(':');
+            return [last, first];
+        })
+        .sort((a, b) => {
+            if (a[0] === b[0]) {
+                return a[1].localeCompare(b[1]);
+            }
+            return a[0].localeCompare(b[0]);
+        })
+        .map(name => `(${name[0]}, ${name[1]})`)
+        .join('');
 }
-console.log(
-    meeting('Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill')
-);
 
-console.log(
-    ['AKU', 'CUMA', 'TAMU', 'GUA'].sort((a, b) => b.charCodeAt() - a.charCodeAt())
-);
+// ===== Unit Tests =====
+function runTests() {
+    const tests = [
+        {
+            input: "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill",
+            expected: "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+        },
+        {
+            input: "Megan:Bell;Amber:Cornwell;Paul:Dorries;James:Dorny;John:Gates;Anna:Korn;Ann:Kern;Alex:Meta;Elizabeth:Russel;Lewis:Steve;Michael:Wahl",
+            expected: "(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)"
+        },
+        {
+            input: "John:Smith;Alice:Smith;Bob:Smith",
+            expected: "(SMITH, ALICE)(SMITH, BOB)(SMITH, JOHN)"
+        }
+    ];
 
-let data = [1, 2, 3, 3, 3, 3, 4]
-data.splice(2, 5)
-console.log(
-    data
-);
+    tests.forEach(({ input, expected }, i) => {
+        const result = meeting(input);
+        console.assert(result === expected, `Test #${i + 1} failed.\nExpected: ${expected}\nGot: ${result}`);
+    });
 
+    console.log("All tests done.");
+}
+
+runTests();
