7d8dca2b1b61ebd2a08daa7f089baa45abce3092
Faezol <fpcyber61@gmail.com>
2025-07-30 09:31:27 +0800

    Day 46/365

---
+++ 2025/Day46-LengthOfSubstring.js
@@ -0,0 +1,29 @@
+// FMP Track Coding Chalenge 2025
+// Day 46/365
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let set = new Set();
+    let left = 0;
+    let maxLength = 0;
+    for (let right = 0; right < s.length; right++) {
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[right]);
+        maxLength = Math.max(maxLength, (right - left + 1));
+    }
+    return maxLength;
+};
+
+console.log(
+    lengthOfLongestSubstring("pwwkew")
+);
+
+
+// Input: s = "pwwkew"
