b3bdb67033533030ed13a7a5b145475211d2e051
Faezol <fpcyber61@gmail.com>
2025-08-18 23:35:15 +0800

    Day 56/365

---
+++ 2025/Day56-containNearbyDuplicate.js
@@ -0,0 +1,29 @@
+// FMP Track Coding Chalenge 2025
+// Day 56/365
+// https://leetcode.com/problems/contains-duplicate-ii/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+    const window = new Set()
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (window.has(nums[i])) return true
+
+        window.add(nums[i])
+
+        if (window.size > k) {
+            window.delete(nums[i - k])
+        }
+    }
+
+    return false
+};
+
+console.log(
+    containsNearbyDuplicate([1, 0, 1, 1], 1)
+);
